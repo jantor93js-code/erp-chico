f6df4513df3655dcbf262aa1b58f74d533922775
Jorge <jantor93.js@gmail.com>
2026-06-22 12:11:14 -0500

    fix: resolve JwtAuthGuard dependency in PMO

--- backend/src/modules/pmo/pmo.module.ts
+++ backend/src/modules/pmo/pmo.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { AuthModule } from '../../auth/auth.module';
 import { TasksService } from './tasks/tasks.service';
 import { TasksController } from './tasks/tasks.controller';
 import { CommentsService } from './comments/comments.service';
 import { CommentsController } from './comments/comments.controller';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [TasksController, CommentsController],
   providers: [TasksService, CommentsService],
   exports: [TasksService, CommentsService],
