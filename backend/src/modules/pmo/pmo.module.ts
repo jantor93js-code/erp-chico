c05730170eb30a1e35603af86c78ac439a982e7b
Jorge <jantor93.js@gmail.com>
2026-06-22 17:36:53 -0500

    backup antes de agregar programId a iniciativas

--- backend/src/modules/pmo/pmo.module.ts
+++ backend/src/modules/pmo/pmo.module.ts
@@ -1,15 +1,38 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { AuthModule } from '../../auth/auth.module';
+
 import { TasksService } from './tasks/tasks.service';
 import { TasksController } from './tasks/tasks.controller';
+
 import { CommentsService } from './comments/comments.service';
 import { CommentsController } from './comments/comments.controller';
 
+import { InitiativesService } from './initiatives/initiatives.service';
+import { InitiativesController } from './initiatives/initiatives.controller';
+
+import { ProjectsService } from './projects/projects.service';
+import { ProjectsController } from './projects/projects.controller';
+
 @Module({
   imports: [PrismaModule, AuthModule],
-  controllers: [TasksController, CommentsController],
-  providers: [TasksService, CommentsService],
-  exports: [TasksService, CommentsService],
+  controllers: [
+    TasksController,
+    CommentsController,
+    InitiativesController,
+    ProjectsController,
+  ],
+  providers: [
+    TasksService,
+    CommentsService,
+    InitiativesService,
+    ProjectsService,
+  ],
+  exports: [
+    TasksService,
+    CommentsService,
+    InitiativesService,
+    ProjectsService,
+  ],
 })
 export class PmoModule {}
